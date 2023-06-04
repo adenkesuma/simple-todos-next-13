77b339e4f721de7a1f5da4a54eff025ec01f6642
adenkesuma <adenfdfd10@gmail.com>
2023-06-04 18:26:06 +0700

    add new link

---
+++ src/app/new/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+interface NewProps {}
+
+const New: FC<NewProps> = () => {
+  return (
+    <h1>New</h1>
+  )
+}
+
+export default New
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
+import New from "./new/page.tsx"
+import Link from "next/link"
 
 interface HomePage {}
 
 const Home: FC<HomePage> = () => {
   return (
     <>
-      <header>
+      <header className="flex justify-between items-center mb-6">
         <h1 className="text-2xl">Todolist</h1>
+        <Link href="/new" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
       </header>
       <ul>
       </ul>
